e64718d6acbfc99fb80476e14799e5edabc4f9d9
Gbengadaniel-15 <dtobi292@gmail.com>
2026-05-29 12:59:44 +0100

    created all database model

---
+++ models/savedJob.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const SavedJobSchema = new mongoose.Schema({
+    applicant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+
+
+    },
+    job: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Job',
+        required: true
+    }
+
+},{timestamps: true})
+
+export default mongoose.model(' SavedJob', SavedJobSchema )
