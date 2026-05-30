d6d7e64b5e72bea11ce135986ae5bf131d079313
Olugbenga Daniel Tobi <dtobi292@gmail.com>
2026-05-30 23:02:52 +0100

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt.js";
 import generateToken from "../utils/generateToken.js";
 
 
@@ -9,7 +9,10 @@ export const registerUser = async (req, res, next) => {
     const userExists = await User.findOne({ email });
 
     if (userExists) {
-      return res.status(400).json({ message: "Email already in use" });
+      return res.status(400).json({ 
+        success: false,
+        message: "Email already in use",
+        data: null });
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -23,15 +26,19 @@ export const registerUser = async (req, res, next) => {
     });
 
     res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
-      token: generateToken(user._id),
+  success: true,
+  message: 'Registration successful',
+  data: {
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token: generateToken(user._id)
+  }
     });
 
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(errorMiddleware);
   }
 };
 
@@ -40,40 +47,68 @@ export const loginUser = async (req, res, next) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(401).json({ message: "Invalid email or password" });
+      return res.status(401).json({ 
+        success: false,
+        message: "Invalid email or password",
+        data: null
+      });
     }
 
     if (user.isBanned) {
-      return res.status(403).json({ message: "Your account has been banned" });
+      return res.status(403).json({
+        success: false,
+        message: "Your account has been banned",
+        data: null
+      });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(401).json({ message: "Invalid email or password" });
+      return res.status(401).json({ 
+        success: false,
+        message: "Invalid email or password",
+        data: null
+      });
     }
 
     res.status(200).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
-      token: generateToken(user._id),
+      success: true,
+      message: 'Login successful',
+      data: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user._id),
+      }
     });
 
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(errorMiddleware);
   }
 };
 
-export const getMe = async (req, res) => {
+// @desc    Get logged in user
+// @route   GET /api/auth/me
+// @access  Private
+
+export const getMe = async (req, res, next) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ 
+        success: false,
+        message: 'User not found',
+        data: null
+      });
     }
-    res.status(200).json(user);
+    res.status(200).json({
+      success: true,
+      message: 'User retrieved successfully',
+      data: user
+  });
 
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(errorMiddleware);
   }
 };
