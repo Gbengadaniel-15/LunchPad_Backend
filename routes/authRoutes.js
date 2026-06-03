3c850ea3ce354d7b9cf09f1ec50b466dbd99403d
Rolajraph <raphaellawrence078@gmail.com>
2026-06-03 10:30:03 +0100

    feat: add auth and user routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { registerUser, loginUser, getMe } from '../controllers/authController.js';
+import { verifyToken } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/me', verifyToken, getMe);
+
+export default router;
